2eb70ef69d83d3c7c10e56d018ddf28e8ce7af50
[Azquiorra] <jjulianto08@gmail.com>
2021-12-30 16:59:12 +0700

    chore: minor update

--- src/config/Routes/index.js
+++ src/config/Routes/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { MainApp, Login, Register } from "../../pages";
+import { MainApp } from "../../pages";
 
 const Routes = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
         <Route path="/">
           <MainApp />
         </Route>
